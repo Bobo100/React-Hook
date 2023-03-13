b0a5110e5e044f18ec3c29cdcec297200bc5900c
Bobo100 <33471758+Bobo100@users.noreply.github.com>
2023-03-13 14:04:44 +0800

    更新useState說明

--- src/pages/component/UseStateFC.tsx
+++ src/pages/component/UseStateFC.tsx
@@ -51,6 +51,10 @@ const UseStateFC = () => {
     //////////////////////////////////////////
     const [count3, setCount3] = useState(() => {
         console.log("initial count3")
+        for (let i = 0; i < 100; i++) {
+            // do nothing
+            console.log("do nothing 3")
+        }
         return 0;
     });
 
@@ -58,11 +62,15 @@ const UseStateFC = () => {
         setCount3(prevState => prevState + 1);
     }
 
-    const [count4, setCount4] = useState(getInitialCount()
-    );
+    const [count4, setCount4] = useState(getInitialCount());
 
     function getInitialCount() {
         console.log("initial count4");
+        for (let i = 0; i < 100; i++) {
+            // do nothing
+            console.log("do nothing 4")
+        }
+
         return 0;
     }
 
@@ -70,6 +78,10 @@ const UseStateFC = () => {
         setCount4(prevState => prevState + 1);
     }
 
+    const [currentTime, setCurrentTime] = useState(() => {
+        const now = new Date();
+        return now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds();
+    });
 
     return (
         <div>
@@ -98,7 +110,7 @@ const UseStateFC = () => {
 
             <p>你可以嘗試看看下面的button</p>
 
-            <p>Count: {count}</p>
+            <p className="hightlight">Count: {count}</p>
             <button onClick={() => setCount(prevState => prevState + 1)}>
                 Increment</button>
             <button onClick={increment}>Increment Version 2</button>
@@ -139,81 +151,57 @@ const UseStateFC = () => {
             </CodeBlockTS>
             <p>這樣就可以正常運作了</p>
             <p>你可以試試看左右兩個button的差別</p>
-            <p>Count: {count2}</p>
+            <p className="hightlight">Count: {count2}</p>
             <button onClick={increment_multiple_error}>Increment Multiple Error</button>
             <button onClick={increment_multiple}>Increment Multiple</button>
 
             <h2>useState初始值 (補充說明)</h2>
-            <p>useState的初始值可以是任何值，包括物件、陣列、函數等等</p>
-            <p>但是要注意的是，如果你的初始值是一個物件或陣列，那麼你在更新state時，你必須要用function in function的方式去更新</p>
-            <p>另外，宣告初始值的時候，如果你的初始值也是很複雜的，那麼也請你用function in function的方式去宣告</p>
 
-            <p>舉例來說，我們有一個物件</p>
-            <CodeBlockTS>
-                {`const obj = { name: 'John', age: 20 };`}
-            </CodeBlockTS>
-            <p>如果我們直接這樣寫 和 我們使用function in function的方式去宣告</p>
-            <CodeBlockTS>
-                {`const [obj, setObj] = useState(obj);
-const [obj, setObj] = useState(() => obj);`}
-            </CodeBlockTS>
-            <p>這兩種方式的結果是不一樣的</p>
-            <p>第一種方式，我們是直接把obj的值給了useState，所以obj的值就會是一個物件</p>
-            <p>第二種方式，我們是把obj的值給了一個function，所以obj的值就會是一個function</p>
+            <p>當然，如果你的初始值是很複雜的，像是計算初始值很耗時或可能引起副作用，例如像從Server端載入資料或本地存儲加載，那麼我們會建議用function in function的方式去宣告，這樣可以保證只會在第一次render時執行一次</p>
 
-            <p>假如因為其他的原因，像是其他的state改變了，導致頁面重新render，這時候第一種寫法就會重新執行一次，而第二種寫法就不會</p>
-            <p>一般來說，我們都會使用function in function的方式去宣告</p>
-            <p>因為這樣可以讓你在每次render時，都可以取得最新的props或state</p>
-
-            {/* <h3>第一種寫法</h3>
-            <div>
-                <p>obj: {person.name}</p>
-                <p>age: {person.age}</p>
-                <button onClick={changeName}>Set Person</button>
-            </div>
-
-            <h3>第二種寫法</h3>
-            <div>
-                <p>obj: {person2.name}</p>
-                <p>age: {person2.age}</p>
-                <button onClick={changeName2}>Set Person</button>
-            </div>
-
-            <p>差別在哪，我們可以用console.log來看一下</p>
-            <p>我們透過更新另一個state來觸發頁面重新render</p>
-            <p>這時候，第一種寫法就會重新執行一次，而第二種寫法就不會</p>
-            <button onClick={() => setCount(prevState => prevState + 1)}>Increment</button> */}
-
-            <p>當然，如果你的初始值是很複雜的，像是計算初始值很耗時或可能引起副作用，例如像從Server端載入資料或本地存儲加載，那麼我們會建議用function in function的方式去宣告</p>
-            <p>這樣可以保證只會在第一次render時執行一次</p>
-            
-        
             <p>舉例來說</p>
             <CodeBlockTS>
-                {`const [count4, setCount4] = useState(getInitialCount()
-    );
-
-    function getInitialCount() {
-        console.log("initial state");
-        return 0;
-    }`}
+                {`const [count4, setCount4] = useState(getInitialCount());
+function getInitialCount() {
+    console.log("initial count4");
+    for (let i = 0; i < 100; i++) {
+        // do nothing
+        console.log("do nothing 4")
+    }
+    return 0;
+}`}
             </CodeBlockTS>
             <p>這樣的話，每次render時，都會執行一次這個function</p>
-            <p>如果你的初始值是一個很複雜的函數，那麼這樣的寫法就會讓你的程式碼變得很難維護</p>
+            <p>如果你的初始值是一個很複雜的函數，那麼這樣的寫法就會導致你的程式碼執行效能變差</p>
             <p>所以我們建議你用function in function的方式去宣告</p>
             <CodeBlockTS>
                 {`const [count3, setCount3] = useState(() => {
-        console.log("initial state")
-        return 0;
-    });`}
+    console.log("initial count3")
+    for (let i = 0; i < 100; i++) {
+        // do nothing
+        console.log("do nothing 3")
+    }
+    return 0;
+});`}
             </CodeBlockTS>
 
             <p>當我們透過button去更新state的時候，就會發現兩個宣告方式的差別</p>
-            <p>請打開console，你會發現第一種寫法，每次render時，都會執行一次這個function，所以我只要更新到其他的state之類的就會導致它重新宣告</p>
-            <p>{count3}</p>
+            <p>請打開console，你會發現第一種寫法，每次render時，都會執行一次這個function，所以我只要更新到其他的state之類的就會導致它重新執行</p>
+            <p>console.log("do nothing4")就會一直執行，會吃效能</p>
+            <p className="hightlight">Count: {count3}</p>
             <button onClick={increment3}>觸發Button count3</button>
-            <p>{count4}</p>
+            <p className="hightlight">Count: {count4}</p>
             <button onClick={increment4}>觸發Button count4</button>
+
+            <p>例如，我們想再渲染進頁面的時候獲得現在的時間</p>
+            <CodeBlockTS>
+                {`const [currentTime, setCurrentTime] = useState(() => {
+    const now = new Date();
+    return now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds();
+});`}
+            </CodeBlockTS>
+            <p>這樣的話，我們就可以在頁面上顯示現在的時間</p>
+            <p className="hightlight">Current Time: {currentTime}</p>
         </div >
     );
 };
