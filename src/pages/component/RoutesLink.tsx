22dd1dd2aeaff6292c0aaf21d3af4a7f235ef67e
Bobo100 <33471758+Bobo100@users.noreply.github.com>
2023-03-23 09:21:09 +0800

    新增active

--- src/pages/component/RoutesLink.tsx
+++ src/pages/component/RoutesLink.tsx
@@ -1,20 +1,20 @@
-import { Link } from "react-router-dom"
+import { NavLink } from "react-router-dom"
 
 const RoutesLink = () => {
     return (
         <>
-            <Link to="/" className='hightlight'>Home</Link>
-            <Link to="/useState" className='hightlight'>useState</Link>
-            <Link to="/useRef" className='hightlight'>useRef</Link>
-            <Link to="/useEffect" className='hightlight'>useEffect</Link>
-            <Link to="/useContext" className='hightlight'>useContext</Link>
-            <Link to="/useReducer" className='hightlight'>useReducer</Link>
-            <Link to="/ContextAndReducerPage" className='hightlight'>useContext和useReducer的差別</Link>
-            <Link to="/useMemo" className='hightlight'>useMemo</Link>
-            <Link to="/useCallback" className='hightlight'>useCallback</Link>
-            <Link to="/CallbackAndMemoPage" className='hightlight'>useMemo和useCallback的差別</Link>
-            <Link to="/useTransition" className='hightlight'>useTransition</Link>
-            <Link to="/useDeferredValue" className='hightlight'>useDeferredValue</Link>
+            <NavLink to="/" className='hightlight'>Home</NavLink>
+            <NavLink to="/useState" className='hightlight'>useState</NavLink>
+            <NavLink to="/useRef" className='hightlight'>useRef</NavLink>
+            <NavLink to="/useEffect" className='hightlight'>useEffect</NavLink>
+            <NavLink to="/useContext" className='hightlight'>useContext</NavLink>
+            <NavLink to="/useReducer" className='hightlight'>useReducer</NavLink>
+            <NavLink to="/ContextAndReducerPage" className='hightlight'>useContext和useReducer的差別</NavLink>
+            <NavLink to="/useMemo" className='hightlight'>useMemo</NavLink>
+            <NavLink to="/useCallback" className='hightlight'>useCallback</NavLink>
+            <NavLink to="/CallbackAndMemoPage" className='hightlight'>useMemo和useCallback的差別</NavLink>
+            <NavLink to="/useTransition" className='hightlight'>useTransition</NavLink>
+            <NavLink to="/useDeferredValue" className='hightlight'>useDeferredValue</NavLink>
 
             <a className='hightlight' href="https://courses.webdevsimplified.com/view/courses/react-hooks-simplified">很棒的教學網站</a>
         </>
